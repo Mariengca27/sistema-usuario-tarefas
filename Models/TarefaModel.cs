4e72669ce4f96b0fd900f186764cb32bf203d8d5
Mariana Machado Amaral <mariengca27@gmail.com>
2023-06-18 19:06:48 -0300

    Método Get Inicial + configuração pacotes NuGet

---
+++ Models/TarefaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public int Status { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿## Baixar o ASP.NET Web API com .NET 6.0 
+## Todos os pacotes NuGet devem ser instalados com a versão 6.0.0
